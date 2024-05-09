1419f9ad8b801121c05c9ae638ce575af744dc3c
Mx Nitro <95213910+Devilharsha@users.noreply.github.com>
2024-05-09 12:24:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("Updates🥵", url= "https://t.me/+7qtsOTaiUA8xZThl"),
+                    InlineKeyboardButton("🔒Support", callback_data = "close")
                 ]
             ]
         )
