d0c0b786137278480281819f7e12c4b655f89edb
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-08-26 10:28:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7190461144:AAFg9CMtI1bmTgPTqDQFAgHiCYhv5ny-jr0")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7190461144:AAH_U2FHdq603jRhIxobQPoRM5Ty6Gx2c9w)
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "7931165"))
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(7350197183)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
