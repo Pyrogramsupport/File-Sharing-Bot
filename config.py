fd1d39ef14857563020527ade512fa5f487304e3
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-08-26 10:31:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7190461144:AAH_U2FHdq603jRhIxobQPoRM5Ty6Gx2c9w)
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7190461144:AAH_U2FHdq603jRhIxobQPoRM5Ty6Gx2c9w")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "7931165"))
